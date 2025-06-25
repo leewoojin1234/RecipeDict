e01dc2fb34328b1960a553d6455ae03a4d3d3507
최장우 <andyfryhaha@gmail.com>
2025-06-25 20:29:24 +0900

    feat::post service

--- src/main/java/com/dgsw/recipedict/service/RecipeService.java
+++ src/main/java/com/dgsw/recipedict/service/RecipeService.java
@@ -18,6 +18,24 @@ public class RecipeService {
     @Autowired
     private RecipeRepository recipeRepository;
 
+    public boolean create(RecipeDTO dto) {
+        try {
+            RecipeEntity entity = RecipeEntity.builder()
+                    .title(dto.getTitle())
+                    .description(dto.getDescription())
+                    .ingredients(dto.getIngredients())
+                    .steps(dto.getSteps())
+                    .imageUrl(dto.getImageUrl())
+                    .build();
+
+            recipeRepository.save(entity);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     public boolean updateRecipe(RecipeDTO dto) {
         Optional<RecipeEntity> optional = recipeRepository.findById(dto.getId());
         if (optional.isPresent()) {
