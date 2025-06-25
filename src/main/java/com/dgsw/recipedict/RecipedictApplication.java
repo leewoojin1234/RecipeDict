acd66db1bd31863578496ab143020a39a3ce15ac
leewoojin1234 <geonui542@gmail.com>
2025-06-25 15:57:40 +0900

    dddd

---
+++ src/main/java/com/dgsw/recipedict/RecipedictApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.recipedict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipedictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipedictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgsw/recipedict/RecipedictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.recipedict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipedictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
