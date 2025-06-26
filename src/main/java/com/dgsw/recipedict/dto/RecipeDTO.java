f332ceadb1ebf1439ea6faf48f49f2c44f54ab84
최장우 <andyfryhaha@gmail.com>
2025-06-26 09:30:43 +0900

    feat::post service

--- src/main/java/com/dgsw/recipedict/dto/RecipeDTO.java
+++ src/main/java/com/dgsw/recipedict/dto/RecipeDTO.java
@@ -11,7 +11,6 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class RecipeDTO {
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
     private String title;
     private String description;
